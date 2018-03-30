bd62184132edac107aef597475769ace03c3cb62
tnangel1984 <tnangel1984123@gmail.com>
2018-03-30 10:47:07 -0400

    Connected mongoose. Tested.Working. Added middleware

--- server.js
+++ server.js
@@ -1,11 +1,29 @@
 const express = require('express');
 const app = express();
+const mongoose = require('mongoose');
 
 
+//Controllers
+
+
+//Middleware
+   app.use(express.json());
+   app.use(express.static('public'));
+
+//test Route
 app.get('/', (req, res)=>{
      res.send("Hello world");
 })
 
+
+
+
+//Connections
+const mongoURI='mongodb://localhost:27017/NewDB';
+mongoose.connect(mongoURI);
+mongoose.connection.once('open', ()=>{
+    console.log("Mongo is connected");
+})
 
 app.listen(3000, ()=>{
      console.log("Listening");
