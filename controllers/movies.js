add50860296154dba32d6705e179b50ae32ec857
tnangel1984 <tnangel1984123@gmail.com>
2018-03-30 17:48:02 -0400

    Express routes tested. Working

--- controllers/movies.js
+++ controllers/movies.js
@@ -9,9 +9,9 @@ const Movies = require('../models/movies.js');
 
 router.get('/', (req, res)=>{
   Movies.find({}, (error, allMovies)=>{
-      console.log('test');
-  })
-})
+      res.json(allMovies);
+  });
+});
 
 
 //post
@@ -20,27 +20,33 @@ router.post('/', (req, res)=>{
   Movies.create(req.body, (error, newMovie)=>{
       console.log(req.body);
        res.json(newMovie);
-  })
-})
+  });
+});
 
 //put
 
 router.put('/:id', (req, res)=>{
-
-})
+    Movies.findByIdAndUpdate(req.params.id, req.body, {new:true}, (error, updatedMovie)=>{
+        res.json(updatedMovie);
+    });
+});
 
 //delete
 
 router.delete('/:id', (req, res)=>{
-
-})
+    Movies.findByIdAndRemove(req.params.id,(error, foundMovie)=>{
+        res.json(foundMovie);
+    });
+});
 
 
 //index
 
 router.get('/:id', (req, res)=>{
-
-})
+  Movies.findById(req.params.id, (error, foundMovie)=>{
+      res.json(foundMovie);
+  });
+});
 
 
 module.exports = router;
